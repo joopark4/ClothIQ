e69bc99a156a87998ad73dffe2fb0d1acafca758
EUN YEON <jooparkhappy4@gmail.com>
2025-10-22 14:28:49 +0900

    기본 프로젝트 추가

---
+++ ClothIQ/ClothIQ/ClothIQApp.swift
@@ -0,0 +1,32 @@
+//
+//  ClothIQApp.swift
+//  ClothIQ
+//
+//  Created by EUN YEON on 10/22/25.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct ClothIQApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
---
+++ ClothIQ/ClothIQTests/ClothIQTests.swift
@@ -0,0 +1,16 @@
+//
+//  ClothIQTests.swift
+//  ClothIQTests
+//
+//  Created by EUN YEON on 10/22/25.
+//
+
+import Testing
+
+struct ClothIQTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
